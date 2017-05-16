337092a39582f1ccd41a5b33be08bc795cb62435
Michael Murray <michaelmurray6298@gmail.com>
2017-05-16 16:16:35 -0700

    reset point. now using classes

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 $(document).ready(function(){
 
-	$(".radar").append("<img>");
-	console.log(document)
-
-	// const request = require('request-promise');
-	const URL = "http://api.wunderground.com/api/18baf686d82d4e8b/animatedradar/q/CA/Truckee.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50"; // TODO Rename better
-
-	let request = fetch(URL)
-	  .then(response => response.blob())
-		.then(appendToRadarImage);
-
-		// .then(function(imageBlob) {
-		//   document.querySelector('.radar img').src = URL.createObjectURL(imageBlob);
-		// });
+// 	$(".radar").append("<img>");
+// 	console.log(document)
+//
+// 	// const request = require('request-promise');
+// 	const radarURL = "http://api.wunderground.com/api/18baf686d82d4e8b/animatedradar/q/CA/Truckee.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50";
+// 	const squawWeatherURL ="http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/Olympic_Valley.json"
+// 	const kirkwoodURL = "http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/Kirkwood.json"
+// 	let city = 'Tahoe '
+// 	const heavenlyURL = `http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/${city}.json`;
+// 	let requestSquawWeather = fetch(squawWeatherURL)
+// 	.then(response => response.text())
+// 	.then(JSON.parse)
+// 	.then(appendSquawWeather)
+//
+// 	// let requestKirkwoodWeather = fetch(kirkwoodURL)
+// 	//
+// 	// let requestHeavenlyWeather =  fetch(heavenlyURL)
+//
+// 	let requestRadar = fetch(radarURL)
+// 	  .then(response => response.blob())
+// 		.then(appendToRadarImage);
+//
+//
+//
+//
+// })
+// function appendToRadarImage(blob) {
+// 	return document.querySelector('.radar img').src = URL.createObjectURL(blob);
+// }
+// function appendSquawWeather(text) {
+// 	console.log(text);
+// 	return $('.squawWeather').text(text.current_observation.wind_mph)
+// }
+// function appendKirkwoodWeather(text) {
+// 	return $('.radar img').
+// }
+// function appendHeavenlyWeather(text) {
+// 	return $('.radar img')
 })
-
-function appendToRadarImage(blob) {
-	return document.querySelector('.radar img').src = URL.createObjectURL(blob);
-}
