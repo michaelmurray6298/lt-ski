262c5caef8283fee9122b4e84ae5df47d9e070f0
Michael Murray <michaelmurray6298@gmail.com>
2017-05-17 10:07:34 -0700

    creates Weather class, all html pages are current

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
 $(document).ready(function(){
+	class WeatherData{
+		constructor(city){
+			this.city = city;
+			this.getWeatherInfo =
+				fetch(`http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/${this.city}.json`)
+					.then(response => response.text())
+					.then(JSON.parse);
+			this.displayWeatherInfo =
+			this.getWeatherInfo
+			.then(response => {
+				this.html = $('<div>');
+				console.log(response.current_observation.feelslike_c);
+			})
+		}
 
-// 	$(".radar").append("<img>");
-// 	console.log(document)
-//
-// 	// const request = require('request-promise');
-// 	const radarURL = "http://api.wunderground.com/api/18baf686d82d4e8b/animatedradar/q/CA/Truckee.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50";
-// 	const squawWeatherURL ="http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/Olympic_Valley.json"
-// 	const kirkwoodURL = "http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/Kirkwood.json"
-// 	let city = 'Tahoe '
-// 	const heavenlyURL = `http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/${city}.json`;
-// 	let requestSquawWeather = fetch(squawWeatherURL)
-// 	.then(response => response.text())
-// 	.then(JSON.parse)
-// 	.then(appendSquawWeather)
-//
-// 	// let requestKirkwoodWeather = fetch(kirkwoodURL)
-// 	//
-// 	// let requestHeavenlyWeather =  fetch(heavenlyURL)
-//
-// 	let requestRadar = fetch(radarURL)
-// 	  .then(response => response.blob())
-// 		.then(appendToRadarImage);
-//
-//
-//
-//
-// })
-// function appendToRadarImage(blob) {
-// 	return document.querySelector('.radar img').src = URL.createObjectURL(blob);
-// }
-// function appendSquawWeather(text) {
-// 	console.log(text);
-// 	return $('.squawWeather').text(text.current_observation.wind_mph)
-// }
-// function appendKirkwoodWeather(text) {
-// 	return $('.radar img').
-// }
-// function appendHeavenlyWeather(text) {
-// 	return $('.radar img')
+		// createRadar(){
+		// 	let url = "http://api.wunderground.com/api/18baf686d82d4e8b/animatedradar/q/CA/Truckee.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50"
+		// 	let radar = fetch(url)
+	  // 	.then(response => response.blob())
+		// 	.then(appendToRadarImage);
+		// 	function appendToRadarImage(blob){
+		// 		return document.querySelector('.radar img').src = URL.createObjectURL(blob);
+		// 	}
+		// }
+
+	}
+	var squaw = new WeatherData("Olympic_Valley")
+
+	squaw.displayWeatherInfo;
 })
