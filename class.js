7c588f7d3a2283076e0ca62874f0d1f9c7c732b0
Michael Murray <michaelmurray6298@gmail.com>
2017-05-18 12:03:04 -0700

    adds forecast data

--- class.js
+++ class.js
@@ -1,13 +1,13 @@
 class WeatherData{
 	constructor(city){
 		this.city = city;
-		this.getRadar =
-			 fetch("http://api.wunderground.com/api/18baf686d82d4e8b/animatedradar/q/CA/Truckee.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50")
-	  	.then(response => response.blob());
-
-
-		this.displayRadar =
-			this.getRadar
+	}
+	getRadar(){
+		return fetch("http://api.wunderground.com/api/18baf686d82d4e8b/animatedradar/q/CA/Truckee.gif?newmaps=1&timelabel=1&timelabel.y=10&num=5&delay=50")
+			.then(response => response.blob());
+	}
+	displayRadar(){
+		return this.getRadar()
 			.then(response => {
 				this.radar = $("<div>").addClass('radar');
 				var img = $('<img>')
@@ -15,24 +15,96 @@ class WeatherData{
 				this.radar.append(img)
 				$('main').append(this.radar);
 			});
-
-
-		this.getWeatherInfo =
-			fetch(`http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/${this.city}.json`)
-				.then(response => response.text())
-				.then(JSON.parse);
-
-
-		this.displayWeatherInfo =
-		this.getWeatherInfo
-		.then(response => {
-			console.log(response.current_observation);
+	}
+	getWeatherInfo(){
+		return fetch(`http://api.wunderground.com/api/18baf686d82d4e8b/conditions/q/CA/${this.city}.json`)
+			.then(response => response.text())
+			.then(JSON.parse)
+			.then(data => data)
+	}
+	displayWeatherInfo(){
+		return this.getWeatherInfo().then(response => {
 			this.currentWeather = $('<div>').addClass('currentWeather');
+			this.currentWeather.append($('<div>').text(("Current Condtions:")));
 			this.currentWeather.append($('<div>').text((response.current_observation.display_location.full)));
 			this.currentWeather.append($('<div>').text((response.current_observation.weather)));
 			this.currentWeather.append($('<div>').text((response.current_observation.temp_f)));
-			this.currentWeather.append($('<div>').text((response.current_observation.wid_dir)));
+			this.currentWeather.append($('<div>').text((response.current_observation.wind_dir)));
 			$('main').append(this.currentWeather)
+		})
+	}
+	getForecastInfo(){
+		return fetch(`http://api.wunderground.com/api/18baf686d82d4e8b/forecast10day/q/CA/${this.city}.json`)
+			.then(response => response.text())
+			.then(JSON.parse)
+			.then(data => data)
+	}
+	displayForecastInfo(){
+		return this.getForecastInfo().then(response => {
+			console.log(response.forecast.simpleforecast.forecastday[0]);
+			this.forecast = $('<div>').addClass('forecast');
+			this.forecast.append($('<div>').text(("10 Day Forecast:")));
+			this.forecast.append($('<br>'));
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[0].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[0].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[0].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[0].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[1].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[1].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[1].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[1].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[2].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[2].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[2].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[2].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[3].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[3].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[3].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[3].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[4].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[4].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[4].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[4].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[5].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[5].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[5].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[5].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[6].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[6].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[6].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[6].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[7].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[7].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[7].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[7].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[8].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[8].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[8].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[8].snow_allday.in + " in.")));
+			this.forecast.append($('<br>'));
+
+			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[9].date.pretty)));
+			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[9].high.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[9].low.fahrenheit + "ºF")));
+			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[9].snow_allday.in + " in.")));
+
+			$('main').append(this.forecast)
 		})
 	}
 }
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-	$('#dropdown1 li').click(function(){
-		var $this = $(this);
-		var selKeyVal = $this.attr('value');
-		var resort = new WeatherData(selKeyVal)
-		resort.displayWeatherInfo;
-		resort.displayRadar;
- 	})
-})
+// $(document).ready(function(){
+// 	$('#dropdown1 li').click(function(){
+// 		var $this = $(this);
+// 		var selKeyVal = $this.attr('value');
+// // 		var resort = new WeatherData(selKeyVal)
+// // 		console.log(resort);
+// // 		// resort.displayWeatherInfo;
+// // 		// resort.displayRadar;
+//   	})
+//   })
