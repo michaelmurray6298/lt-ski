04eb3f44d48049d37a5c565fa1bac49c86097a10
Michael Murray <michaelmurray6298@gmail.com>
2017-05-18 16:13:42 -0700

    adds twitter feed, formats html

--- class.js
+++ class.js
@@ -13,7 +13,7 @@ class WeatherData{
 				var img = $('<img>')
 				img.attr('src', URL.createObjectURL(response))
 				this.radar.append(img)
-				$('main').append(this.radar);
+				$('.radarContainer').append(this.radar);
 			});
 	}
 	getWeatherInfo(){
@@ -28,9 +28,9 @@ class WeatherData{
 			this.currentWeather.append($('<div>').text(("Current Condtions:")));
 			this.currentWeather.append($('<div>').text((response.current_observation.display_location.full)));
 			this.currentWeather.append($('<div>').text((response.current_observation.weather)));
-			this.currentWeather.append($('<div>').text((response.current_observation.temp_f)));
-			this.currentWeather.append($('<div>').text((response.current_observation.wind_dir)));
-			$('main').append(this.currentWeather)
+			this.currentWeather.append($('<div>').text((response.current_observation.temp_f + "ºF")));
+			this.currentWeather.append($('<div>').text(("Wind: " + response.current_observation.wind_mph +" mph. " + response.current_observation.wind_dir)));
+			$('.current_weatherContainer').append(this.currentWeather)
 		})
 	}
 	getForecastInfo(){
@@ -41,70 +41,80 @@ class WeatherData{
 	}
 	displayForecastInfo(){
 		return this.getForecastInfo().then(response => {
-			console.log(response.forecast.simpleforecast.forecastday[0]);
 			this.forecast = $('<div>').addClass('forecast');
+			this.forecast1 =$('<div>').addClass("day1_container")
+			this.forecast2 =$('<div>').addClass("day2_container")
+			this.forecast3 =$('<div>').addClass("day3_container")
+			this.forecast4 =$('<div>').addClass("day4_container")
+			this.forecast5 =$('<div>').addClass("day5_container")
+			this.forecast6 =$('<div>').addClass("day6_container")
+			this.forecast7 =$('<div>').addClass("day7_container")
+			this.forecast8 =$('<div>').addClass("day8_container")
+			this.forecast9 =$('<div>').addClass("day9_container")
+			this.forecast10 =$('<div>').addClass("day10_container")
+
 			this.forecast.append($('<div>').text(("10 Day Forecast:")));
-			this.forecast.append($('<br>'));
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[0].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[0].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[0].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[0].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast1.append($('<div>').addClass("day_container").text((response.forecast.simpleforecast.forecastday[0].date.pretty)));
+			this.forecast1.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[0].high.fahrenheit + "ºF")));
+			this.forecast1.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[0].low.fahrenheit + "ºF")));
+			this.forecast1.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[0].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[1].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[1].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[1].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[1].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast2.append($('<div>').text((response.forecast.simpleforecast.forecastday[1].date.pretty)));
+			this.forecast2.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[1].high.fahrenheit + "ºF")));
+			this.forecast2.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[1].low.fahrenheit + "ºF")));
+			this.forecast2.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[1].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[2].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[2].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[2].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[2].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast3.append($('<div>').text((response.forecast.simpleforecast.forecastday[2].date.pretty)));
+			this.forecast3.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[2].high.fahrenheit + "ºF")));
+			this.forecast3.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[2].low.fahrenheit + "ºF")));
+			this.forecast3.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[2].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[3].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[3].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[3].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[3].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast4.append($('<div>').text((response.forecast.simpleforecast.forecastday[3].date.pretty)));
+			this.forecast4.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[3].high.fahrenheit + "ºF")));
+			this.forecast4.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[3].low.fahrenheit + "ºF")));
+			this.forecast4.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[3].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[4].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[4].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[4].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[4].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast5.append($('<div>').text((response.forecast.simpleforecast.forecastday[4].date.pretty)));
+			this.forecast5.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[4].high.fahrenheit + "ºF")));
+			this.forecast5.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[4].low.fahrenheit + "ºF")));
+			this.forecast5.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[4].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[5].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[5].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[5].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[5].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast6.append($('<div>').text((response.forecast.simpleforecast.forecastday[5].date.pretty)));
+			this.forecast6.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[5].high.fahrenheit + "ºF")));
+			this.forecast6.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[5].low.fahrenheit + "ºF")));
+			this.forecast6.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[5].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[6].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[6].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[6].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[6].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast7.append($('<div>').text((response.forecast.simpleforecast.forecastday[6].date.pretty)));
+			this.forecast7.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[6].high.fahrenheit + "ºF")));
+			this.forecast7.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[6].low.fahrenheit + "ºF")));
+			this.forecast7.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[6].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[7].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[7].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[7].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[7].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast8.append($('<div>').text((response.forecast.simpleforecast.forecastday[7].date.pretty)));
+			this.forecast8.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[7].high.fahrenheit + "ºF")));
+			this.forecast8.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[7].low.fahrenheit + "ºF")));
+			this.forecast8.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[7].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[8].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[8].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[8].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[8].snow_allday.in + " in.")));
-			this.forecast.append($('<br>'));
+			this.forecast9.append($('<div>').text((response.forecast.simpleforecast.forecastday[8].date.pretty)));
+			this.forecast9.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[8].high.fahrenheit + "ºF")));
+			this.forecast9.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[8].low.fahrenheit + "ºF")));
+			this.forecast9.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[8].snow_allday.in + " in.")));
 
-			this.forecast.append($('<div>').text((response.forecast.simpleforecast.forecastday[9].date.pretty)));
-			this.forecast.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[9].high.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[9].low.fahrenheit + "ºF")));
-			this.forecast.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[9].snow_allday.in + " in.")));
-
-			$('main').append(this.forecast)
+			this.forecast10.append($('<div>').text((response.forecast.simpleforecast.forecastday[9].date.pretty)));
+			this.forecast10.append($('<div>').text(("High: " + response.forecast.simpleforecast.forecastday[9].high.fahrenheit + "ºF")));
+			this.forecast10.append($('<div>').text(("Low: " + response.forecast.simpleforecast.forecastday[9].low.fahrenheit + "ºF")));
+			this.forecast10.append($('<div>').text(("Snow: " + response.forecast.simpleforecast.forecastday[9].snow_allday.in + " in.")));
+			this.forecast.append(this.forecast1)
+			this.forecast.append(this.forecast2)
+			this.forecast.append(this.forecast3)
+			this.forecast.append(this.forecast4)
+			this.forecast.append(this.forecast5)
+			this.forecast.append(this.forecast6)
+			this.forecast.append(this.forecast7)
+			this.forecast.append(this.forecast8)
+			this.forecast.append(this.forecast9)
+			this.forecast.append(this.forecast10)
+			$('.forecastContainer').append(this.forecast)
+			console.log(document)
 		})
 	}
 }
---
+++ init.js
@@ -0,0 +1,4 @@
+var resort = new WeatherData("Olympic_Valley")
+resort.displayRadar();
+resort.displayWeatherInfo();
+resort.displayForecastInfo();
